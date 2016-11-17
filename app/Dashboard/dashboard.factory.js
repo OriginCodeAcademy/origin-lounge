d184f46547e8a77240a1cc8891c000d609ec66c8
Exponent500 <reuvenkishon@gmail.com>
2016-11-17 09:43:57 -0800

    added comments, refactored code, added roles to local storage, and added ability to populate markdown editor with content from DB

--- app/Dashboard/dashboard.factory.js
+++ app/Dashboard/dashboard.factory.js
@@ -5,99 +5,241 @@
     .module('app')
     .factory('DashboardFactory', DashboardFactory);
 
-    DashboardFactory.$inject = ['$http', '$q', 'storageFactory'];
+    DashboardFactory.$inject = ['$http', '$q', 'storageFactory', 'originAPIBaseURL'];
 
-    function DashboardFactory($http, $q, storageFactory) {
+    function DashboardFactory($http, $q, storageFactory, originAPIBaseURL) {
         var service = {
             getCategory: getCategory,
             postCategory: postCategory,
             editCategory: editCategory,
             getContent: getContent,
-            postContent: postContent
+            postContent: postContent,
+            getRoles: getRoles
             // editContent: editContent
         };
         return service;
 
-        ////////////////
-
+        // get all Categories from mongoDB
         function getCategory() {
-            return $http({
+            
+            var defer = $q.defer();
+
+            $http({
                 method:'GET',
                 url:'http://localhost:3000/api/category'
             })
+
             .then(function(response){
-                return response;
-            })
+
+                if (typeof response.data === "object") {
+
+                    defer.resolve(response.data);
+                
+                } else {
+
+                    defer.reject(response);
+                }
+    
+            },
+
+            function(error){
+
+                defer.reject(error);
+
+            });
+
+            return defer.promise;
         }
 
+        // post a Category to the mongoDB
         function postCategory(catName){
-              // creates the params portion of the POST URL
+
+              var defer = $q.defer();
+              
+              // creates the body portion of the POST URL
               var info = 'name=' + catName;
-              return $http({
+
+              $http({
                 method:'POST',
                 url:'http://localhost:3000/api/category',
                 data: info,
                 headers: {
                     'Content-Type' : 'application/x-www-form-urlencoded'
                 }
-                //form:{'name':catName }
+
             })
-              .then(function(response){
-                console.log("successfully added the" +catName+ "from factory to database");
-                return response;
-            })
-          }
+
+            .then(function(response){
+
+                if (typeof response.data === "object") {
+
+                    defer.resolve(response.data);
+                
+                } else {
+
+                    defer.reject(response);
+                }
+    
+            },
+
+            function(error){
+
+                defer.reject(error);
+
+            });
+
+            return defer.promise;
+        }
+
+          // edit a Category in the mongoDB
           function editCategory(info){
-            return $http({
+
+            var defer = $q.defer();
+
+            $http({
                 method:'PUT',
                 url:'http://localhost:3000/api/category' + info.id,
                 data: info,
                 headers: {
                     'Content-Type' : 'application/x-www-form-urlencoded'
                 }
-                //form:{'name':catName }
+
             })
+
             .then(function(response){
-                console.log("successfully added the" +catName+ "from factory to database");
-                return response;
-            }),
-            function(error){
-                return error;
-            }
-        }
-    //////////////////////////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////////////////////
-             function getContent() {
-            return $http({
-                method:'GET',
-                url:'http://localhost:3000/api/content'
-            })
-            .then(function(response){
-                return response.data;
-            })
-        }
-            function postContent(title, body){
-              // creates the params portion of the POST URL
-              var userName = storageFactory.getLocalStorage('userSession').user.userName;
-              var info = 'title=' + title + '&bodyDescr=' + body + '&createdBy=' + userName;
-              return $http({
-                method:'POST',
-                url:'http://localhost:3000/api/content',
-                data: info,
-                headers: {
-                    'Content-Type' : 'application/x-www-form-urlencoded'
+
+                if (typeof response.data === "object") {
+
+                    defer.resolve(response.data);
+                
+                } else {
+
+                    defer.reject(response);
                 }
-                //form:{'name':catName }
+    
+            },
+
+            function(error){
+
+                defer.reject(error);
+
+            });
+
+            return defer.promise;
+        }
+
+
+         // get a Content entry in the mongoDB, by content ID
+         function getContent(incoming) {
+
+            var defer = $q.defer();
+
+            $http({
+                method:'GET',
+                url:'http://localhost:3000/api/content/'+incoming
             })
-              .then(function(response){
-                console.log("successfully added the" + title + "from factory to database");
-                return response;
+
+            .then(function(response){
+
+                if (typeof response.data === "object") {
+
+                    defer.resolve(response.data);
+                
+                } else {
+
+                    defer.reject(response);
+                }
+    
+            },
+
+            function(error){
+
+                defer.reject(error);
+
+            });
+
+            return defer.promise;
+        }
+
+        // post a new Content entry into the mongoDB
+        function postContent(title, body){
+
+          var defer = $q.defer();
+          
+          // grab logged in user's username from localstorage
+          var userName = storageFactory.getLocalStorage('userSession').user.userName;
+          
+          // create the body portion of the POST URL
+          var info = 'title=' + title + '&bodyDescr=' + body + '&createdBy=' + userName;
+          
+          $http({
+            method:'POST',
+            url:'http://localhost:3000/api/content',
+            data: info,
+            headers: {
+                'Content-Type' : 'application/x-www-form-urlencoded'
+            }
+
+        })
+            .then(function(response){
+
+                if (typeof response.data === "object") {
+
+                    defer.resolve(response.data);
+                
+                } else {
+
+                    defer.reject(response);
+                }
+    
+            },
+
+            function(error){
+
+                defer.reject(error);
+
+            });
+
+            return defer.promise;
+        }
+
+          // get all roles from origin.api 
+          function getRoles() {
+
+            var defer = $q.defer();
+
+            var token = storageFactory.getLocalStorage('userSession').token;
+
+            $http({
+                method: 'GET',
+                url: originAPIBaseURL + 'api/roles',
+                headers: {
+                    'Authorization' : 'Bearer ' + token
+                }
             })
+
+            .then(function(response){
+
+                if (typeof response.data === "object") {
+
+                    storageFactory.setLocalStorage('roles', response.data);
+                    defer.resolve(response.data);
+                
+                } else {
+
+                    defer.reject(response);
+                }
+
+            },
+
+            function(error){
+
+                defer.reject(error);
+
+            });
+
+            return defer.promise;
           }
-
-        
-
-
 
     }
 })();
